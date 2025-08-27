2cdaf72f26cc8eb7b71fab8030efc4e6bc84581f
NiKar-coder <Nikitakarataev22@gmail.com>
2025-08-27 22:58:04 +0300

    perf: App optimized

--- mainWindow.py
+++ mainWindow.py
@@ -4,6 +4,7 @@ from PyQt6.QtCore import pyqtSlot
 from PyQt6.QtGui import QPixmap, QKeySequence, QShortcut
 from editor import Editor
 import shutil
+#
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
